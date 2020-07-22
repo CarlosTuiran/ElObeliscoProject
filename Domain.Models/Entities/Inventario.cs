2bbab32108f30b7e2c28922d9f7c9d955ae55cb2
CarlosTuiran <Ctuiran@users.noreply.github.com>
2020-07-21 23:45:33 -0500

    Registrar Producto

--- Domain.Models/Entities/Inventario.cs
+++ Domain.Models/Entities/Inventario.cs
@@ -7,7 +7,7 @@ namespace Domain.Models.Entities
     public class Inventario
     {
         private double id { get; set; }
-        public int Referencia { get; set; }
+        public string Referencia { get; set; }
         public int Cantidad { get; set; }
         public string Bodega { get; set; }
         public Inventario()
--- Domain.Models/Entities/Producto.cs
+++ Domain.Models/Entities/Producto.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using static Domain.Models.Base.BaseEntity;
 
 namespace Domain.Models.Entities
 {
-    public class Producto
+    public class Producto : Entity<int>
     {
-        public int Referencia { get; set; }
+        public string Referencia { get; set; }
         public int Descripcion { get; set; }
         public string FormatoVenta { get; set; }
         public string Marca { get; set; }
@@ -17,8 +18,22 @@ namespace Domain.Models.Entities
         public DateTime FechaRegistro { get; set; }
         public Producto()
         {
-
+            this.FechaRegistro = DateTime.Today;
         }
-
+        public IReadOnlyList<string> CanCrear(Producto producto)
+        {
+            var errors = new List<string>();
+            if (string.IsNullOrEmpty(producto.Referencia))
+                errors.Add("Campo Referencia vacio");
+            if (string.IsNullOrEmpty(producto.FormatoVenta))
+                errors.Add("Campo FormatoVenta vacio");
+            if (producto.Costo < 0)
+                errors.Add("Campo Costo negativo");
+            if(producto.PrecioVenta<0)
+                errors.Add("Campo PrecioVenta negativo");
+            if (producto.FechaRegistro == null)
+                errors.Add("Campo FechaRegistro vacio");
+            return errors;
+        }
     }
 }
