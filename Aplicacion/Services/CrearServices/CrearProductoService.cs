7b51437dc275df296ab18b6fd00ab92ddcabdc51
CarlosTuiran <Ctuiran@users.noreply.github.com>
2020-07-21 23:46:55 -0500

    Registrar Producto

---
+++ Aplicacion/Services/CrearServices/CrearProductoService.cs
@@ -0,0 +1,51 @@
+﻿using Aplicacion.Request;
+using Domain.Models.Contracts;
+using Domain.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Aplicacion.Services.CrearServices
+{
+    public class CrearProductoService
+    {
+        readonly IUnitOfWork _unitOfWork;
+
+        public CrearProductoService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public CrearProductoResponse Ejecutar(CrearProductoRequest request)
+        {
+
+            var producto = _unitOfWork.ProductoServiceRepository.FindFirstOrDefault(t => t.Referencia == request.Referencia);
+            if (producto == null)
+            {
+                Producto newProducto = new Producto(request.Referencia, request.Descripcion, request.FormatoVenta, request.Marca, request.Fabrica,
+                    request.Costo, request.PrecioVenta, request.IVA);
+                IReadOnlyList<string> errors = newProducto.CanCrear(newProducto);
+                if (errors.Any())
+                {
+                    string listaErrors = "Errores:";
+                    foreach (var item in errors)
+                    {
+                        listaErrors += item.ToString();
+                    }
+                    return new CrearProductoResponse() { Message = listaErrors };
+                }
+                else
+                {
+                    _unitOfWork.ProductoServiceRepository.Add(newProducto);
+                    _unitOfWork.Commit();
+                    return new CrearProductoResponse() { Message = $"Producto Creado Exitosamente" };
+                }
+            }
+            else
+            {
+                return new CrearProductoResponse() { Message = $"Producto ya existe revise la referencia de este" };
+            }
+        }
+    }
+}
--- Domain.Models/Entities/Producto.cs
+++ Domain.Models/Entities/Producto.cs
@@ -8,7 +8,7 @@ namespace Domain.Models.Entities
     public class Producto : Entity<int>
     {
         public string Referencia { get; set; }
-        public int Descripcion { get; set; }
+        public string Descripcion { get; set; }
         public string FormatoVenta { get; set; }
         public string Marca { get; set; }
         public string Fabrica { get; set; }
@@ -16,10 +16,20 @@ namespace Domain.Models.Entities
         public double PrecioVenta { get; set; }
         public double IVA { get; set; }
         public DateTime FechaRegistro { get; set; }
-        public Producto()
+        public Producto(string referencia, string descripcion, string formatoventa, string marca ,string fabrica,
+            double costo, double precioventa, double iva)
         {
-            this.FechaRegistro = DateTime.Today;
+            this.Referencia = referencia;
+            this.Descripcion = descripcion;
+            this.FormatoVenta = formatoventa;
+            this.Marca = marca;
+            this.Fabrica = fabrica;
+            this.Costo = costo;
+            this.PrecioVenta = precioventa;
+            this.IVA = iva;
+            this.FechaRegistro = DateTime.Now;
         }
+
         public IReadOnlyList<string> CanCrear(Producto producto)
         {
             var errors = new List<string>();
@@ -29,11 +39,11 @@ namespace Domain.Models.Entities
                 errors.Add("Campo FormatoVenta vacio");
             if (producto.Costo < 0)
                 errors.Add("Campo Costo negativo");
-            if(producto.PrecioVenta<0)
+            if (producto.PrecioVenta < 0)
                 errors.Add("Campo PrecioVenta negativo");
             if (producto.FechaRegistro == null)
                 errors.Add("Campo FechaRegistro vacio");
             return errors;
         }
     }
-}
+ }
