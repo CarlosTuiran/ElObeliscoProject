91b2c94203a09ebfd330d845105aed2c7d933b72
AgamezRaul <39956182+AgamezRaul@users.noreply.github.com>
2020-08-08 17:15:49 -0500

    Clase tiempo

---
+++ Domain.Models/Entities/Tiempo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Models.Entities
+{
+    public class Tiempo
+    {
+		public DateTime Fecha { get; set; }
+		public int Año { get; set; }
+		public int Mes { get; set; }
+		public int Dia { get; set; }
+		public int DiaDelAño { get; set; }
+		public int SemanaDelAño { get; set; }
+		public int Trimestre { get; set; }
+		public int Semestre { get; set; }
+		public string NombreMes { get; set; }
+		public string NombreMesCorto { get; set; }
+		public string NombreDia { get; set; }
+		public string NombreDiaCorto { get; set; }
+	}
+}
