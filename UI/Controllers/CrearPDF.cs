717693234cf23f8828fa649f1bc97c4912e3cb4c
AgamezRaul <39956182+AgamezRaul@users.noreply.github.com>
2021-05-12 22:14:53 -0500

    Mas Pdf

--- UI/Controllers/CrearPDF.cs
+++ UI/Controllers/CrearPDF.cs
@@ -1,4 +1,6 @@
-﻿using Infra.Datos;
+﻿using Aplicacion.Services.ConsultarServices;
+using Domain.Models.Contracts;
+using Infra.Datos;
 using Infra.Datos.Base;
 using Microsoft.AspNetCore.Mvc;
 using Rotativa.AspNetCore;
@@ -11,24 +13,53 @@ namespace UI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class CrearPDF : ControllerBase
+    public class CrearPDF : Controller
     {
         private readonly ObeliscoContext _context;
-
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ConsultarFacturaCompra consultarFacturaCompra;
         public CrearPDF(ObeliscoContext context)
         {
             _context = context;
+            _unitOfWork = new UnitOfWork(_context);
+            consultarFacturaCompra = new ConsultarFacturaCompra(_context, _unitOfWork);
         }
 
         [HttpGet]
         public IActionResult Index()
         {
             var resultado = _context.Usuario.FirstOrDefault(u => u.Id == 1).Nombre;
-            return new ViewAsPdf("Index"){
-                Model=resultado
+
+            PDFResponse rta = new PDFResponse
+            {
+                Name = resultado,
+                Name2 = "Raul2.0+1.0"
+            };
+            return new ViewAsPdf("EgresoDiario"){
+                Model=rta
+               
             };
            
         }
+        [HttpGet("/d")]
+        public IActionResult EgresoDiario(int facturaId)
+        {
+            //ViewData.egreso=
+            ViewData["NombreMov"]="Egreso Diario";
+            ViewBag.NombreMov="Egreso Diario";
+            ViewData["Codigo"]="G-2020-12";
+            return View();
+        }
+        [HttpGet("/dd")]
+        public IActionResult FacturaCompra(int facturaId)
+        {
+            return View();
+        }
+    }
+    public class PDFResponse{
+        public string Name { get; set; }
+        public string Name2 { get; set; }        
+        
         
     }
 }
